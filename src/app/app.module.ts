d67ab7c9a565e48da0e7b7f542c527d72e293664
Shazibali Gesawat <shazibali@icloud.com>
2018-06-10 00:53:41 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,43 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { EmailValidator } from 'ng-email-validation';
+import { ValidatorProvider } from '../providers/validator/validator';
+import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Login } from '../pages/login/login';
+import { Signup } from '../pages/signup/signup';
+
+import { AuthService } from '../providers/auth-service/auth-service';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    Login,
+    Signup
+
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    Login,
+    Signup
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    EmailValidator,
+    ValidatorProvider,
+    AuthService,
+
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
